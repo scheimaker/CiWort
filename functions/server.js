572f11e1b4d25408c7cb6ca5ca95b98dad034908
Sicheng Zhu <sczhu1990@hotmail.com>
2025-01-10 09:01:58 +0100

    update the server.js bugs

--- functions/server.js
+++ functions/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const path = require('path');
+
 // const admin = require('firebase-admin');
 const authRoutes = require('./routes/authRoutes'); // Correct import of authRoutes
 const wordRoutes = require('./routes/wordRoutes');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const functions = require('firebase-functions');
-
+const serviceAccount = require('./firebaseServiceAccountKey.json');
 const admin = require('firebase-admin');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://<your-database-name>.firebaseio.com' // Replace with your database URL
-});
+  databaseURL: 'https://ciwort.firebaseapp.com' // Replace with your database URL
+},"CiWort");
 
 const db = admin.firestore(); // Initialize Firestore
 
@@ -43,10 +45,10 @@ app.get('*', (req, res) => {
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 
 exports.app = functions.https.onRequest(app);
